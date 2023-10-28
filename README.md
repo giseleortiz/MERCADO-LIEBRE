6e56e0f956c8f06e3c490263c315f77b98dad1d5
gisele ortiz <giseleortizuriel@gmail.com>
2023-10-28 05:16:21 -0300

    agregue style

---
+++ README.md
@@ -0,0 +1,12 @@
+# Trabajo Practico ML - Maquetado
+## Comision 22 
+### Gisele Ortiz
+------------------------------
+# Estructura HTML.
+### 1er entrega fecha 16/10/23 
+-------------------------------
+# Agregando estilo.
+### 2da entrega fecha 23/10/23
+
+
+
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require ('express');
-const path = require ('path');
-const app =express();
-const PORT =3030;
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = 3030;
 
-app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'views','home.html')));
+app.use(express.static(path.join(__dirname,'public')));
 
 
-app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`));
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
+
+
+
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
